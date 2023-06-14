27d7526311f0aab2dc343deb03761cb31df62ae2
YE0BLEE <mauyeoblee@gmail.com>
2023-06-14 17:27:54 +0900

    20230614

--- src/main/java/com/lec/controller/RecruitController.java
+++ src/main/java/com/lec/controller/RecruitController.java
@@ -1,10 +1,16 @@
 package com.lec.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.lec.domain.Member;
 import com.lec.domain.Recruit;
 import com.lec.service.RecruitService;
 
@@ -14,9 +20,31 @@ public class RecruitController {
 	@Autowired
 	private RecruitService recruitService;
 	
+	@Autowired
+	Environment environment;
+	
+	@ModelAttribute("member")
+	public Member setMember() {
+		return new Member();
+	}
+		
 	@GetMapping("/recruit")
-	public String getRecruitPage() {
-		return "recruit/recruit";
+	public String getRecruitPage(Model model) {
+		List<Recruit> recruitList = recruitService.getRecruitList();
+	    model.addAttribute("recruitList", recruitList);
+	    return "recruit/recruit";
+	}
+	
+	@GetMapping("/getRecruit")
+	public String getRecruit(Recruit recruit, Model model) {
+		model.addAttribute("recruit", recruitService.getRecruit(recruit));
+		return "recruit/getRecruit";
+	}
+	
+	@GetMapping("/adminRecruitList")
+	public String adminRecruitView(Model model, Recruit recruit) {
+		model.addAttribute("recruit", recruitService.getRecruitList());
+		return "recruit/adminRecruitList";
 	}
 	
 	@GetMapping("/insertRecruit")
@@ -27,7 +55,31 @@ public class RecruitController {
 	@PostMapping("insertRecruit")
 	public String insertRecruit(Recruit recruit) {
 		recruitService.insertRecruit(recruit);
-		return "recruit/recruit";
+		return "redirect:recruit";
+	}
+	
+	@GetMapping("/updateRecruit")
+	public String updateRecruit(Recruit recruit, Model model) {
+		model.addAttribute("works", recruitService.getRecruit(recruit));
+		return "recruit/updateRecruit";
+	}
+	
+	@PostMapping("/updateRecruit")
+	public String updateRecruit(Member member, Recruit recruit) {
+		if(member.getId() == null) {
+			return "redirect:login";
+		}
+		recruitService.updateRecruit(recruit);
+		return "redirect:recruit";
+	}
+	
+	@GetMapping("/deleteRecruit")
+	public String deleteRecruit(Member member, Recruit recruit) {
+		if(member.getId() == null) {
+			return "redirect:login";
+		}
+		recruitService.deleteRecruit(recruit);
+		return "redirect:recruit";
 	}
 	
 
